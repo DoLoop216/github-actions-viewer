23a8b43af07d6cbb66e99fa92f842c7b4265c750
Aleksa Ristic <aristiccitsira@gmail.com>
2025-07-19 11:47:29 -0400

    Drag and drop feature implemented

--- src/code/github-workflows-explorer/constants/generalConstants.js
+++ src/code/github-workflows-explorer/constants/generalConstants.js
@@ -1,13 +1,17 @@
+const vscode = require('vscode')
+
 const DIRECTORY_SEPARATOR = '_'
 const EXPLORER_TREE_ITEM_CONTEXT_VALUE = 'github-workflows-explorer-item'
 const EXPLORER_TREE_ITEM_CONTEXT_VALUE_FILE = 'github-workflows-explorer-item-file'
 const EXPLORER_TREE_ITEM_FOLDER_ICON = 'folder'
 const COULD_NOT_LOAD_WORKFLOWS_EXPLORER = 'Could not load workflows explorer.'
+const WORKFLOWS_PATH = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, ".github", "workflows")
 
 module.exports = {
     DIRECTORY_SEPARATOR,
     EXPLORER_TREE_ITEM_CONTEXT_VALUE,
     EXPLORER_TREE_ITEM_CONTEXT_VALUE_FILE,
     EXPLORER_TREE_ITEM_FOLDER_ICON,
-    COULD_NOT_LOAD_WORKFLOWS_EXPLORER
+    COULD_NOT_LOAD_WORKFLOWS_EXPLORER,
+    WORKFLOWS_PATH
 }
--- src/code/github-workflows-explorer/extension.js
+++ src/code/github-workflows-explorer/extension.js
@@ -2,6 +2,7 @@ const { MainViewTreeProvider } = require('./views/mainView');
 const { NEW_FILE_COMMAND, DELETE_FILE_COMMAND } = require('./constants/commandsConstants');
 const { newFileCommand } = require('./commands/newFileCommand');
 const { deleteFileCommand } = require('./commands/deleteFileCommand');
+const { dragAndDropController } = require('./controllers/dragAndDropController');
 
 const vscode = require('vscode');
 var treeProvider;
@@ -10,15 +11,26 @@ var treeProvider;
  */
 async function activate(context) {
 	treeProvider = new MainViewTreeProvider(context)
-	vscode.window.registerTreeDataProvider('MainView', treeProvider)
+	const treeView = vscode.window.createTreeView('MainView', {
+		treeDataProvider: treeProvider,
+		dragAndDropController: dragAndDropController
+	});
+	context.subscriptions.push(treeView);
 	vscode.commands.registerCommand(NEW_FILE_COMMAND, (item) => newFileCommand(treeProvider, item));
-  vscode.commands.registerCommand(DELETE_FILE_COMMAND, (item) => deleteFileCommand(treeProvider, item));
+  	vscode.commands.registerCommand(DELETE_FILE_COMMAND, (item) => deleteFileCommand(treeProvider, item));
 }
 
 async function deactivate() {}
 
+function getTreeDataProvider() {
+	if (!treeProvider) {
+		throw new Error('Tree data provider is not initialized. Make sure to call activate first.');
+	}
+	return treeProvider;
+}
 
 module.exports = {
 	activate,
-	deactivate
+	deactivate,
+	getTreeDataProvider
 }
